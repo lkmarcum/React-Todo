a96cd3435e9afe512459361f8e4fc5100bb98ef6
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-05 14:21:47 -0500

    set up passing of state to components, data info now being rendered to window

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -3,7 +3,9 @@ import React from "react";
 const Todo = props => {
   return (
     <div>
-      <p />
+      <p>{props.item.task}</p>
     </div>
   );
 };
+
+export default Todo;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,9 +1,16 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
 import React from "react";
+import Todo from "./Todo";
 
 const TodoList = props => {
-  return <div className="todo-list" />;
+  return (
+    <div className="todo-list">
+      {props.list.map(item => (
+        <Todo key={item.id} item={item} />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
